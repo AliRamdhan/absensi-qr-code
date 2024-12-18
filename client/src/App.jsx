570a192b96fcadd5fe0a19907e9aea33fca5ba24
AliRamadhan <aliramdhan4551@gmail.com>
2024-12-19 01:43:24 +0700

    [feat] first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hero from "./pages/Hero";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
